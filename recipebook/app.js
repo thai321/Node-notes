b86aa810ef692880ee181cd20bd4a2842262d264
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-11-04 15:22:43 -0700

    Initiall commit

---
+++ recipebook/app.js
@@ -0,0 +1,34 @@
+const express = require('express'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  cons = require('consolidate'),
+  dust = require('dustjs-helpers'),
+  pg = require('pg'),
+  app = express();
+
+// DB Connect String
+const connect = 'postgres://nodeRecipe:123456@localhost/recipebookdb';
+
+// Assign Dust Engine To .dust Files
+app.engine('dust', cons.dust);
+
+// Set Default Ext .dust
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(__dirname + '/public'));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// Server
+app.listen(3000, function() {
+  console.log('Server Started On Port 3000');
+});
