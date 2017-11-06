aacabeaf5f79c49227f8477811316587f584f18d
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-11-05 18:27:21 -0800

    Add javascript

--- recipebook/app.js
+++ recipebook/app.js
@@ -3,11 +3,23 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   cons = require('consolidate'),
   dust = require('dustjs-helpers'),
-  pg = require('pg'),
+  // pg = require('pg'),
   app = express();
 
+const { Pool, Client } = require('pg');
+const connectionString = 'postgres://nodeRecipe:123456@localhost/recipebookdb';
+
+const pool = new Pool({
+  connectionString: connectionString
+});
+
+const client = new Client({
+  connectionString: connectionString
+});
+client.connect();
+
 // DB Connect String
-const connect = 'postgres://nodeRecipe:123456@localhost/recipebookdb';
+// const conString = 'postgres://nodeRecipe:123456@localhost/recipebookdb';
 
 // Assign Dust Engine To .dust Files
 app.engine('dust', cons.dust);
@@ -25,7 +37,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  pool.connect(function(err, client, done) {
+    if (err) {
+      return console.log('Error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM recipes', function(err, result) {
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.render('index', { recipes: result.rows });
+      done();
+
+      console.log(result);
+    });
+  });
 });
 
 // Server
