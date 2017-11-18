04ecd5200ad3d2019061b0caff69573841d67e79
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-11-17 20:14:33 -0800

    Add String Reversal

--- Interview/AlgoCasts/exercises/reversestring/index.js
+++ Interview/AlgoCasts/exercises/reversestring/index.js
@@ -2,10 +2,43 @@
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((acum, char) => char + acum, '');
+}
 
 module.exports = reverse;
+
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+//
+// function reverse(str) {
+//   if (str === '') return '';
+//   return reverse(str.substring(1)) + str[0];
+// }
+//
+// function reverse(str) {
+//   return reverseHelper(str, '');
+// }
+//
+// function reverseHelper(str, acum) {
+//   if (str.length <= 0) return acum;
+//   return reverseHelper(str.substring(1), str[0] + acum);
+// }
+//
+// function reverse(str) {
+//   let reversed = '';
+//
+//   for (let char of str) {
+//     reversed = char + reversed;
+//   }
+//
+//   return reversed;
+// }
