61ebf2f24adbc2288721b1c679cd0d888307cf0a
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-11-18 12:48:29 -0800

    Add Pyramind solution # 1 interative

--- Interview/AlgoCasts/exercises/pyramid/index.js
+++ Interview/AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,22 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2 * n - 1) / 2);
+
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < 2 * n - 1; column++) {
+      if (column >= mid - row && column <= mid + row) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
