385500df01ecaaccff9ee7cef1f21b3d67d5503a
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2018-01-17 21:56:31 -0800

    Add buidl JS framework

---
+++ JS-understand-the-weird-parts/J2_StructuringSafeCode/Starter/Greetr.js
@@ -0,0 +1,107 @@
+(function(global, $) {
+  var Greetr = function(firstname, lastname, language) {
+    return new Greetr.init(firstname, lastname, language);
+  };
+
+  var supportedLangs = ['en', 'es'];
+
+  var greetings = {
+    en: 'Hello',
+    es: 'Hola'
+  };
+
+  var formalGreetings = {
+    en: 'Greetings',
+    es: 'Saludos'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    es: 'Inicio sesion'
+  };
+
+  Greetr.prototype = {
+    fullName: function() {
+      return this.firstname + ' ' + this.lastname;
+    },
+
+    validate: function() {
+      if (supportedLangs.indexOf(this.language) === -1) {
+        throw 'Invalid language';
+      }
+    },
+
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstname + '!';
+    },
+
+    formalGreetings: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+
+    greet: function(formal) {
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreetings();
+      } else {
+        msg = this.greeting();
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+
+      return this;
+    },
+
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+
+      return this;
+    },
+
+    setLang: function(lang) {
+      this.language = lang;
+
+      this.validate();
+
+      return this;
+    },
+
+    HTMLGreeting: function(selector, formal) {
+      if (!$) {
+        throw 'jQuery not loaded';
+      }
+
+      if (!selector) {
+        throw 'Missing jQuery selector';
+      }
+
+      var msg;
+      if (formal) {
+        msg = this.formalGreetings();
+      } else {
+        msg = this.greeting();
+      }
+
+      $(selector).html(msg);
+
+      return this;
+    }
+  };
+
+  Greetr.init = function(firstname, lastname, language) {
+    var self = this;
+    self.firstname = firstname || '';
+    self.lastname = lastname || '';
+    self.language = language || 'en';
+
+    self.validate();
+  };
+
+  Greetr.init.prototype = Greetr.prototype;
+  global.Greetr = global.G$ = Greetr;
+})(window, jQuery);
---
+++ JS-understand-the-weird-parts/J2_StructuringSafeCode/Starter/app.js
@@ -0,0 +1,16 @@
+var g = G$('John', 'Doe');
+g
+  .greet()
+  .setLang('es')
+  .greet(true);
+
+$('#login').click(function() {
+  var loginGrtr = G$('John', 'Doe');
+
+  $('#logindiv').hide();
+
+  loginGrtr
+    .setLang($('#lang').val())
+    .HTMLGreeting('#greeting', true)
+    .log();
+});
